feb470574d05c98e3236fab49b1da03dbad09c33
saniyafatima07 <saniyafatima200631@gmail.com>
2025-08-23 02:20:00 +0530

    Minor navbar fixes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,11 +6,7 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="w-full flex items-center justify-between px-8 py-6 bg-white dark:bg-gray-900 shadow-md fixed top-0 z-50">
-      <div className="flex items-center gap-2 text-blue-700 dark:text-blue-400 font-bold text-2xl">
-        🔐 CyberAware
-      </div>
-
+    <nav className="w-full flex items-center justify-start px-8 py-6 bg-white dark:bg-gray-900 shadow-md fixed top-0 z-50">
       <div className="hidden md:flex space-x-6 font-medium text-gray-600 dark:text-gray-300">
         <a href="/" className="hover:text-blue-600 dark:hover:text-blue-400">
           Home
@@ -18,7 +14,10 @@ export default function Navbar() {
         <a href="/" className="hover:text-blue-600 dark:hover:text-blue-400">
           Learn
         </a>
-        <a href="tips" className="hover:text-blue-600 dark:hover:text-blue-400">
+        <a
+          href="/tips"
+          className="hover:text-blue-600 dark:hover:text-blue-400"
+        >
           Tips
         </a>
         <a
@@ -38,20 +37,23 @@ export default function Navbar() {
 
       {menuOpen && (
         <div className="justify-start absolute top-16 left space-x-3 -0 w-full bg-white dark:bg-gray-900 shadow-md flex flex-col items-start px-8 py-4 space-y-4 md:hidden">
-          <a href="#home" onClick={() => setMenuOpen(false)}>
+          <a href="/" onClick={() => setMenuOpen(false)}>
             Home
           </a>
-          <a href="#learn" onClick={() => setMenuOpen(false)}>
+          <a href="/" onClick={() => setMenuOpen(false)}>
             Learn
           </a>
-          <a href="#tips" onClick={() => setMenuOpen(false)}>
+          <a href="/tips" onClick={() => setMenuOpen(false)}>
             Tips
           </a>
-          <a href="#about" onClick={() => setMenuOpen(false)}>
+          <a href="/about" onClick={() => setMenuOpen(false)}>
             About
           </a>
         </div>
       )}
+      <div className="flex items-center ml-auto gap-2 text-blue-700 dark:text-blue-400 font-bold text-2xl">
+        🔐 CyberAware
+      </div>
     </nav>
   );
 }
